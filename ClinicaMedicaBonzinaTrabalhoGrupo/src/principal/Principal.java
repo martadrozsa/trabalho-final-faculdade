4075339c13e480fccc6f4a01f87fa5093f878e1d
Antônio Sgarbi <antonio.sgarbi@hotmail.com>
2021-07-04 06:12:06 -0300

    chamada da telaPrincipal adicionada

--- ClinicaMedicaBonzinaTrabalhoGrupo/src/principal/Principal.java
+++ ClinicaMedicaBonzinaTrabalhoGrupo/src/principal/Principal.java
@@ -1,10 +1,12 @@
 
 package principal;
 
+import view.telaPrincipal.TelaPrincipal;
+
 public class Principal {
     
     public static void main(String[] args) {
-        
+       TelaPrincipal tela = new TelaPrincipal();
     }
     
 }
